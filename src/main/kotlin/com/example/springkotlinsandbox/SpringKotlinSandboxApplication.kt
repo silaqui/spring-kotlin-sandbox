25d0a5358492a1977715e11121498f60d29e7e3b
Miki <aivi.pol@gmail.com>
2022-09-01 18:02:19 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/springkotlinsandbox/SpringKotlinSandboxApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springkotlinsandbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinSandboxApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinSandboxApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springkotlinsandbox/SpringKotlinSandboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springkotlinsandbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinSandboxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
